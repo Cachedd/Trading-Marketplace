19f3726ad927c4a42051a037a73ce16f024b4367
Corey Santarossa <103389809@student.swin.edu.au>
2023-08-29 11:50:21 +1000

    updated transaction history

--- Frontend/src/components/TransactionHistory.jsx
+++ Frontend/src/components/TransactionHistory.jsx
@@ -5,11 +5,11 @@ import { AiOutlineClose } from "react-icons/ai";
 const TransactionHistory = () => {
   return (
     <div className="fixed inset-0 h-full w-full bg-gray-400 bg-opacity-70 flex justify-center items-center">
-      <div className="w-[90%] lg:w-[70%] h-[70%] bg-[#3A71FD] flex flex-col z-50 items-center justify-center rounded-xl text-white relative">
-        <h1 className="w-full text-center p-5 font-semibold">
+      <div className="w-[90%] lg:w-[70%] h-[70%] bg-[#FFFFFF] flex flex-col z-50 items-center justify-center rounded-xl text-[#3A71FD] relative">
+        <h1 className="w-full text-center p-5 font-semibold text-black">
           Transaction History
         </h1>
-        <AiOutlineClose className="absolute top-5 right-5 text-2xl cursor-pointer" />
+        <AiOutlineClose className="absolute top-5 right-5 text-black text-2xl cursor-pointer" />
 
         <ul className="flex justify-start items-center flex-col h-full w-full">
           <Transactions />
--- Frontend/src/components/Transactions.jsx
+++ Frontend/src/components/Transactions.jsx
@@ -3,9 +3,12 @@ import React from "react";
 const Transactions = () => {
   return (
     <div className="h-16 flex justify-center items-center w-full px-5 gap-5 lg:gap-24 font-semibold text-sm border-b-2 border-gray-500">
-      <h1>Time</h1>
-      <h1>0x000sfge0wfjh...</h1>
-      <a href="#"> See Explorer</a>
+      <h1 className="w-[33%] text-center">Time</h1>
+      <h1 className="w-[33%] text-center overflow-x-auto">0x000sfge0wfjh...</h1>
+      <a href="#" className="w-[33%] text-center">
+        {" "}
+        See Explorer
+      </a>
     </div>
   );
 };
